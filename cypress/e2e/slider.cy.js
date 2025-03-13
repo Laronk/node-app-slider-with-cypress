78b935968b16e7e61cb92bcca13edf37665c130f
Karpiuk Jan <38437109+Laronk@users.noreply.github.com>
2025-03-13 10:14:53 +0100

    Add new cypress test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,22 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+// ---- NEW TESTS ----
+
+// User can navigate through slides using navigation buttons
+describe('Swiper Gallery Test', function () {
+  it('Check slider navigation using buttons', function () {
+    cy.visit('http://localhost:3000');
+    // check if first slide is Rome
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    // previous slide should be Paris (slider wraparound functionlaity - backwards)
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    // next slide should again be Rome (wraparound functionality - forward)
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
